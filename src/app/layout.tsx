ff8b6e514d0a7558098ad0c09daaa43884dbee74
vinyl810 <19thsummer@kaist.ac.kr>
2024-08-22 23:33:07 +0900

    chore: organize components directory with server and client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import Header from '@/components/common/Header';
+import Header from '@/components/server/common/Header';
 
 interface RootLayoutProps {
   readonly children: React.ReactNode;
