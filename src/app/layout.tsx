eea3b0496e9e81092b7a688c0d85deaa1f55c078
judahwon <hwondaa@gmail.com>
2024-09-28 19:39:44 +0900

    fix: 페이지 로드 시 theme로 인한 깜빡임 문제 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
-import { headers } from 'next/headers';
 import './globals.css';
 import Header from '@/components/server/common/Header';
 import Footer from '@/components/server/common/Footer';
+import { cookies } from 'next/dist/client/components/headers';
 
 interface RootLayoutProps {
   readonly children: React.ReactNode;
@@ -14,11 +14,11 @@ export const metadata: Metadata = {
 };
 
 const RootLayout = ({ children }: RootLayoutProps) => {
-  const themeHeader = headers().get('OS-Theme');
-  const isDarkMode = themeHeader === 'dark';
+  const themeCookieName = process.env.NEXT_PUBLIC_THEME_COOKIE_NAME || '';
+  const themeCookie = cookies().get(themeCookieName)?.value;
 
   return (
-    <html lang='en' className={isDarkMode ? 'dark' : ''}>
+    <html lang='en' className={themeCookie === 'light' ? '' : 'dark'}>
       <body className='bg-theme text-theme'>
         <Header />
         <main className='mt-[64px]'>{children}</main>
