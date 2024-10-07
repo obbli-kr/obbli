fed487d1989e48c993d323b8dbb47f50553e25e2
vinyl810 <19thsummer@kaist.ac.kr>
2024-10-07 23:27:49 +0900

    feat: 배열 복제 없는 무한 스크롤 테스트

--- src/components/client/CarouselClientWrapper.tsx
+++ src/components/client/CarouselClientWrapper.tsx
@@ -8,130 +8,39 @@ interface CarouselClientWrapperProps {
   itemWidth: number;
 }
 
-const CarouselClientWrapper = ({ children, itemCount, itemWidth }: CarouselClientWrapperProps) => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const [isPaused, setIsPaused] = useState(false);
-  const [touchStart, setTouchStart] = useState(0);
-  const animationRef = useRef<number | null>(null);
-  const lastTimeRef = useRef<number | null>(null);
-  const touchMoveRef = useRef<number>(0);
+const revolveSpeed = 1; // 2px/s
+
+const CarouselClientWrapper = ({ children, itemWidth }: CarouselClientWrapperProps) => {
+  const [time, setTime] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const animationFrameRef = useRef<number | null>(null);
 
   // 무한 스크롤을 위한 children 복제 (pc 크기상 2개면 충분)
-  const childrenArray = React.Children.toArray(children);
-  const duplicatedChildren = [...childrenArray, ...childrenArray];
+  const [childrenArray, setChildrenArray] = useState(React.Children.toArray(children));
+  const itemCount = childrenArray.length;
+  const scrollPosition = time;
 
-  // 애니메이션 함수
-  const animate = useCallback((time: number) => {
-    if (lastTimeRef.current != null && !isPaused) {
-      const deltaTime = time - lastTimeRef.current;
-      const totalWidth = itemWidth * itemCount;
+  const animate = useCallback((timeStamp: number) => {
+    setTime((time) => time + revolveSpeed);
+    animationFrameRef.current = requestAnimationFrame(animate);
+    console.log(timeStamp);
+  }, []);
 
-      setScrollPosition((prevPosition) => {
-        const speed = 100; // 100px/s
-        const newPosition = (prevPosition + ((speed * deltaTime) / 1000)) % totalWidth;
-        return newPosition >= 0 ? newPosition : newPosition + totalWidth;
-      });
-    }
-    lastTimeRef.current = time;
-    animationRef.current = requestAnimationFrame(animate);
-  }, [itemCount, itemWidth, isPaused]);
-
-  // 애니메이션 시작/중지 함수
-  const startAnimation = useCallback(() => {
-    if (!animationRef.current) {
-      lastTimeRef.current = performance.now();
-      animationRef.current = requestAnimationFrame(animate);
-    }
+  useEffect(() => {
+    animationFrameRef.current = requestAnimationFrame(animate);
+    return () => {
+      if (animationFrameRef.current) {
+        cancelAnimationFrame(animationFrameRef.current);
+      }
+    };
   }, [animate]);
 
-  const stopAnimation = useCallback(() => {
-    if (animationRef.current) {
-      cancelAnimationFrame(animationRef.current);
-      animationRef.current = null;
-    }
-  }, []);
-
   useEffect(() => {
-    // 모바일에서 포스터 클릭으로 링크 들어갔다가 뒤로가기하면 애니메이션이 멈추는 문제 해결
-    const handleVisibilityChange = () => {
-      if (document.hidden) {
-        stopAnimation();
-      } else {
-        setIsPaused(false);
-        startAnimation();
-      }
-    };
-
-    const handlePopState = () => {
-      setIsPaused(false);
-      startAnimation();
-    };
-
-    document.addEventListener('visibilitychange', handleVisibilityChange);
-    window.addEventListener('popstate', handlePopState);
-
-    startAnimation();
-
-    return () => {
-      stopAnimation();
-      document.removeEventListener('visibilitychange', handleVisibilityChange);
-      window.removeEventListener('popstate', handlePopState);
-    };
-  }, [startAnimation, stopAnimation]);
-
-  useEffect(() => {
-    if (isPaused) {
-      stopAnimation();
-    } else {
-      startAnimation();
+    if (scrollPosition && scrollPosition % itemWidth === 0) {
+      setChildrenArray((prev) => [...prev.slice(1), prev[0]]);
+      setTime((time) => time - itemWidth);
     }
-    return () => {
-      stopAnimation();
-    };
-  }, [isPaused, startAnimation, stopAnimation]);
-
-  // 터치 이벤트 핸들러(모바일)
-  const handleTouchStart = useCallback((e: TouchEvent<HTMLDivElement>) => {
-    setIsPaused(true);
-    setTouchStart(e.targetTouches[0].clientX);
-    touchMoveRef.current = 0;
-  }, []);
-
-  const startTouchPosition = useCallback((e: TouchEvent<HTMLDivElement>) => {
-    const currentTouchPosition = e.targetTouches[0].clientX;
-    const diff = touchStart - currentTouchPosition;
-    touchMoveRef.current = diff;
-
-    setScrollPosition((prevPosition) => {
-      let newPosition = prevPosition + diff;
-      const totalWidth = (210 + 24) * itemCount;
-
-      if (newPosition < 0) {
-        newPosition += totalWidth;
-      } else if (newPosition >= totalWidth) {
-        newPosition -= totalWidth;
-      }
-
-      return newPosition;
-    });
-
-    setTouchStart(currentTouchPosition);
-  }, [touchStart, itemCount]);
-
-  const handleTouchEnd = useCallback(() => {
-    setIsPaused(false);
-    const snapThreshold = itemWidth / 2;
-    const mod = scrollPosition % itemWidth;
-
-    if (Math.abs(mod) > snapThreshold) {
-      setScrollPosition((prevPosition) => {
-        const direction = mod > 0 ? 1 : -1;
-        return prevPosition + ((itemWidth - Math.abs(mod)) * direction);
-      });
-    } else {
-      setScrollPosition((prevPosition) => prevPosition - mod);
-    }
-  }, [itemWidth, scrollPosition]);
+  }, [scrollPosition, itemWidth, itemCount]);
 
   return (
     <div
@@ -141,13 +50,9 @@ const CarouselClientWrapper = ({ children, itemCount, itemWidth }: CarouselClien
         scrollbarWidth: 'none',
         msOverflowStyle: 'none',
       }}
-      onMouseEnter={() => setIsPaused(true)}
-      onMouseLeave={() => setIsPaused(false)}
-      onTouchStart={handleTouchStart}
-      onTouchMove={startTouchPosition}
-      onTouchEnd={handleTouchEnd}
       role="region"
       aria-label="이미지 캐러셀(슬라이드)"
+      ref={containerRef}
     >
       <div
         className='relative flex'
@@ -155,7 +60,7 @@ const CarouselClientWrapper = ({ children, itemCount, itemWidth }: CarouselClien
           transform: `translateX(-${ scrollPosition }px)`,
         }}
       >
-        {duplicatedChildren.map((child, index) => (
+        {childrenArray.map((child, index) => (
           <div
             key={index}
             className='mx-3 h-[297px] w-[210px] shrink-0 shadow-lg'
